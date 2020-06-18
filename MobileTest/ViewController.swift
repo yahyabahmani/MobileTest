0946933674cb373547d1d463fded54ce8d2e81e0
macbook <yahya.bahmani16@gmail.com>
2020-06-18 20:05:50 +0300

    Initial Commit

---
+++ MobileTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileTest
+//
+//  Created by macbook on 3/29/1399 AP.
+//  Copyright © 1399 Yahya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
